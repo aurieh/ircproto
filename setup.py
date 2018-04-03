d75c36c495cc7545ec603a7e1eb0470b9bb1fdcf
aurieh <me@aurieh.me>
2018-04-03 18:40:00 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='ircproto',
+    description='ircproto - protocol-only IRC library',
+    long_description='Protocol-only, bring your own IO IRC library for Python.',
+    license='BSD',
+    author='Aurieh',
+    url='https://github.com/aurieh/ircproto',
+    packages=['ircproto'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Communications :: Chat :: Internet Relay Chat'
+    ])
